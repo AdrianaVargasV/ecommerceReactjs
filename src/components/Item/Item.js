9b2fabb8d048f726c12b20d72582df8ea19d48a7
Adriana Vargas V <adrivv3.0@gmail.com>
2023-01-26 21:14:09 -0600

    PreEntrega2Vargas

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,21 +1,13 @@
 import { Link } from "react-router-dom"
 
-<<<<<<< HEAD
 const Item = ({ id, name, img, price }) => {
-=======
-const Item = ({ name, img, price }) => {
->>>>>>> d265e81e84dec220732769e013fb111c8beda7f1
     return (
         <div>
             <div >
             <h3>{name}</h3>
             <img src={img} alt={name} style={{ width: 150 }} />
             <p>Precio: ${price}</p>
-<<<<<<< HEAD
                 <Link to={`/item/${id}`}><button>ver más</button></Link>
-=======
-                <Link to={'/item/${id}'}><button>ver más</button></Link>
->>>>>>> d265e81e84dec220732769e013fb111c8beda7f1
             </div>
 
         </div>
