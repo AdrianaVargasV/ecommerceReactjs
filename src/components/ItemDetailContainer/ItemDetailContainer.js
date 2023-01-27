87eef7cc37e8fc1eb8938a16ad278517c420c154
Adriana Vargas V <adrivv3.0@gmail.com>
2023-01-26 21:12:16 -0600

    PreEntrega2Vargas

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,10 +1,7 @@
 import { useParams } from "react-router-dom";
 import { getProductById } from "../../asyncMock";
 import { useState, useEffect } from "react";
-<<<<<<< HEAD
 import ItemDetail from "../ItemDetail/ItemDetail";
-=======
->>>>>>> d265e81e84dec220732769e013fb111c8beda7f1
 
 const ItemDetailContainer = () => {
     const [product, setProduct] = useState({})
@@ -20,11 +17,7 @@ const ItemDetailContainer = () => {
     return (
         <div>
             <h2>Detalle del producto</h2>
-<<<<<<< HEAD
             <ItemDetail {...product}/>
-=======
-            <h2>{product?.name}</h2>
->>>>>>> d265e81e84dec220732769e013fb111c8beda7f1
         </div>
     )
 }
